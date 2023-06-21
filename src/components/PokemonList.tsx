f18599b1bdf77bc0827a16c3ecd897e5a9b81b3a
Jochem van Hout <jochem@Jochems-Laptop.home>
2023-06-21 22:56:24 +0200

    commented code example

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -10,4 +10,30 @@ console.log(pokemonData);
   )
 }
   
-export default PokemonList
+// export default PokemonList
+
+
+// const fetchData = async (url, callback) => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+
+//   callback(data);
+// };
+
+// const PokemonList = ({ infoUrl }) => {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     fetchData(infoUrl, setData);
+//   }, []);
+
+//   return (
+//     <React.Fragment>
+//       <h1>{name}</h1>
+
+//       {data !== null && <img src={data?.sprites?.front_default} alt="front_default" />}
+//     </React.Fragment>
+//   ) 
+// };
+
+// export default PokemonList;
